4677fc62246fa407e0ae4ba7a922b42394499a00
cedriclankeu <cedriclankeu2@yahoo.fr>
2019-04-16 18:52:13 +0200

    reset

---
+++ LabJavaSmtp/src/main/java/ch/heigvd/res/labsmtp/impl/Application.java
@@ -0,0 +1,21 @@
+package ch.heigvd.res.labsmtp.impl;
+
+
+import java.util.logging.Logger;
+
+import org.apache.commons.io.FileUtils;
+
+/**
+ * @author Olivier Liechti
+ */
+public class Application  {
+
+
+
+    private static final Logger LOG = Logger.getLogger(Application.class.getName());
+
+    public static void main(String[] args) {
+        LOG.info("Labo SMTP");
+
+    }
+}
