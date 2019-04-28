b55793062d3135e549c865007917da4f688eedc8
cedriclankeu <cedriclankeu2@yahoo.fr>
2019-04-28 04:39:56 +0200

    smtp_structure

---
+++ LabJavaSmtp/src/main/java/ch/heigvd/res/labsmtp/impl/Application.java
@@ -0,0 +1,22 @@
+package ch.heigvd.res.labsmtp.impl;
+
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+
+
+
+
+public class Application {
+
+    private static final Logger LOG = Logger.getLogger(Application.class.getName());
+
+    public static void main(String[] args) throws IOException {
+
+        LOG.info("Labo SMTP");
+
+
+    }
+
+}
